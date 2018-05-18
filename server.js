a54d0dc44444e413d3e2c9597a0058ef9e3c7074
CharithWi <CharithW@99x.lk>
2018-05-18 10:58:53 +0530

    changes to get from the device

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ import logger from "./core/logger/app-logger";
 import session from "express-session";
 import config from "./core/config/config.dev";
 import connectToDb from "./db/connect";
+import { Server } from "ws";
+
+const wss = new Server({ port: 7171 });
 
 // const session = require("express-session");
 
@@ -30,11 +33,11 @@ const app = express();
 const server = http.createServer(app);
 const socketio = io.listen(server);
 
-app.use(filter());
+// app.use(filter());
 
-app.use(helmet.xssFilter());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.noSniff());
+// app.use(helmet.xssFilter());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.noSniff());
 
 app.use(session({ secret: "prezuracx" })); // session secret
 app.use(passport.initialize());
@@ -55,26 +58,74 @@ server.listen(port, () => {
   logger.info("server started - ", port);
 });
 
+let clients = [];
+
+wss.on("connection", function connection(ws, req) {
+  const ip = req.connection.remoteAddress;
+  ws.on("message", data => {
+    console.log("67890", data);
+    for (let i = 0; i < clients.length; i++) {
+      const dataArray = JSON.parse(data);
+
+      const pressureData = [];
+      // for (let i = 0; i < dataArray.length; i++) {
+      pressureData.push({
+        value: dataArray[0] / 4,
+        x: 60,
+        y: 200
+      });
+
+      pressureData.push({
+        value: dataArray[1] / 4,
+        x: 40,
+        y: 100
+      });
+
+      pressureData.push({
+        value: dataArray[2] / 4,
+        x: 80,
+        y: 90
+      });
+
+      pressureData.push({
+        value: dataArray[3] / 4,
+        x: 60,
+        y: 150
+      });
+      // }
+      clients[i].emit("data", {
+        data: pressureData
+      });
+      // console.log("data", data);
+    }
+  });
+});
+
 socketio.on("connection", socket => {
-  console.log(socket);
+  clients.push(socket);
+
+  // console.log(socket);
   let connected = true;
   console.log("joined");
   socket.on("disconnect", () => {
     connected = false;
     socketio.emit("disconnected", { user: "user-disconnected" });
     console.log("Disconnected");
+
+    clients.splice(clients.indexOf(socket), 1);
   });
 
-  socket.on("getData", () => {
-    const intervalID = setInterval(() => {
-      if (connected) {
-        socketio.emit("data", { data: generatePoints() });
-        console.log("Heat map data sent");
-      } else {
-        clearInterval(intervalID);
-      }
-    }, 1200);
-  });
+  // socket.on("getData", () => {
+  //   const intervalID = setInterval(() => {
+  //     if (connected) {
+  //       socketio.emit("data", { data: generatePoints() });
+
+  //       console.log("Heat map data sent");
+  //     } else {
+  //       clearInterval(intervalID);
+  //     }
+  //   }, 1200);
+  // });
 
   socket.on("getPressureData", () => {
     const intervalID = setInterval(() => {
@@ -107,6 +158,8 @@ let generatePoints = () => {
   }
   return points;
 };
+
+let calculateTheSurroundNodePressure = data => {};
 
 let getRandomInt = max => {
   return Math.floor(Math.random() * Math.floor(max));
